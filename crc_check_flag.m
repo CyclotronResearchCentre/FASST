09aaa2b40fdcf7df6170d2acdc6705abd7ecf556
Christophe Phillips <c.phillips@uliege.be>
2018-06-12 10:54:16 +0200

    Improved flag_check function

--- crc_check_flag.m
+++ crc_check_flag.m
@@ -1,4 +1,4 @@
-function flags = crc_check_flag(flags_o,flags)
+function [flags,ind_repl] = crc_check_flag(flags_o,flags,opt)
 
 % FORMAT flags = crc_check_flag(flags_o,flags)
 %
@@ -14,20 +14,44 @@ function flags = crc_check_flag(flags_o,flags)
 % OUPUT:
 % flags     filled flag/option structure
 %__________________________________________________________________________
-% Copyright (C) 2009 Cyclotron Research Centre
+% Copyright (C) 2015 Cyclotron Research Centre
 
 % Written by Y. Leclercq & C. Phillips, 2008.
+% Revised and updated by C. Phillips, 2015
 % Cyclotron Research Centre, University of Liege, Belgium
-% $Id$
+
+% Option structure:
+% opt
+% .verbose : print out information [true] or not [false, def.] on the fixed
+%            structure
+
+if nargin<3
+    opt.verbose = false;
+end
 
 f_names = fieldnames(flags_o);
 % list fields in default structure
 
 Nfields = length(f_names);
+ind_repl = zeros(1,Nfields);
 for ii=1:Nfields
-    if ~isfield(flags,f_names{ii}) || isempty(getfield(flags,f_names{ii}))
-        flags = setfield(flags,f_names{ii},getfield(flags_o,f_names{ii}));
+    % Update the output if
+    % - a field is missing
+    % - the field is empty when it shouldn't
+    if ~isfield(flags,f_names{ii}) || ...
+            ( isempty(flags.(f_names{ii})) && ~isempty(flags_o.(f_names{ii})) )
+        flags.(f_names{ii}) = flags_o.(f_names{ii});
+        ind_repl(ii) = 1;
+        if opt.verbose
+            fprintf('\n\tAdding field ''%s'' to structure ''%s''.', ...
+                f_names{ii},inputname(2));
+            jump_line = true;
+        end
     end
 end
 
+if opt.verbose && jump_line
+    fprintf('\n');
+end
 
+end
